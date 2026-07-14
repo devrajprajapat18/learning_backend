a996259c27da1954f044bc13b18143bf7134cf9e
Devraj <prajapatdevraj18@gmail.com>
2026-07-14 21:52:28 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -17,4 +17,8 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
-export {app}
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users",userRouter)
+//http://localhost:8000/api/v1/users/register
+export default app
